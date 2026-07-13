932c42747df302544c4000e7ab0c4b21eb89a356
shahab <ashahhab2727@gmail.com>
2026-07-13 23:24:14 +0330

    update database settings

--- shine_star_project/settings.py
+++ shine_star_project/settings.py
@@ -12,19 +12,28 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path 
 import os
+import environ  
+
+env = environ.Env(
+
+    DEBUG=(bool, False)
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o5*x-x(nnfd)f-&2+czt^c98-4cl#75mf89@q@+ebrde!yrp#^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
@@ -75,10 +84,7 @@ WSGI_APPLICATION = 'shine_star_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
