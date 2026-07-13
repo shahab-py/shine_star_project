9eace16cf3cd87bf52bc57640fd22f0d3aff5450
shahab <ashahhab2727@gmail.com>
2026-07-13 18:33:20 +0330

    Configure project to use PostgreSQL

--- shine_star_project/settings.py
+++ shine_star_project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'shine_star_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shine_star_db',
+        'USER': 'postgres',
+        'PASSWORD': '202780301105',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
