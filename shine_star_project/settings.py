7f6f20971a91806f2a58d23827909c290ee858cb
shahab <ashahhab2727@gmail.com>
2026-07-13 18:54:19 +0330

    Configure project to use PostgreSQL

--- shine_star_project/settings.py
+++ shine_star_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o5*x-x(nnfd)f-&2+czt^c98-4cl#75mf89@q@+ebrde!yrp#^'
+
+load_dotenv() # این خط فایل .env را می‌خواند
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
